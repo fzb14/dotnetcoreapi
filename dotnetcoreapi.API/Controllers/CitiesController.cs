7e051ee21edd9de0063353f13cdf6281739534de
fzb14 <fzb14@hotmail.com>
2020-09-11 01:46:27 +0800

    api return first data

---
+++ dotnetcoreapi.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcoreapi.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id=1, cityName="New York"},
+                    new {id=2, cityName="Tokyo"}
+                }
+            );
+        }
+    }
+}
--- dotnetcoreapi.API/Startup.cs
+++ dotnetcoreapi.API/Startup.cs
@@ -16,6 +16,7 @@ namespace dotnetcoreapi.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => { options.EnableEndpointRouting = false; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,14 +28,14 @@ namespace dotnetcoreapi.API
             }
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseMvc();
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
