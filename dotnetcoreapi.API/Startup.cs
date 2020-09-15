89b37e41b623ed8d426409951bd21ae120d957a8
fzb14 <fzb14@hotmail.com>
2020-09-15 13:35:28 +0800

    add xml output formatter when the request header: {Accept:application/xml}

--- dotnetcoreapi.API/Startup.cs
+++ dotnetcoreapi.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using json=Newtonsoft.Json.Serialization;
 
 namespace dotnetcoreapi.API
@@ -17,7 +18,10 @@ namespace dotnetcoreapi.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => { options.EnableEndpointRouting = false; })
+            services.AddMvc(options => { 
+                options.EnableEndpointRouting = false;
+                options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            })
             //services.AddMvc()
                 .AddJsonOptions(o => {
                     //o.JsonSerializerOptions.DictionaryKeyPolicy = null;
