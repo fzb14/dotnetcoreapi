40179783ad1e6bc3a878b133a59d146125aa501b
fzb14 <fzb14@hotmail.com>
2020-09-15 13:12:02 +0800

    change json format

--- dotnetcoreapi.API/Startup.cs
+++ dotnetcoreapi.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using json=Newtonsoft.Json.Serialization;
 
 namespace dotnetcoreapi.API
 {
@@ -16,7 +17,13 @@ namespace dotnetcoreapi.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => { options.EnableEndpointRouting = false; });
+            services.AddMvc(options => { options.EnableEndpointRouting = false; })
+            //services.AddMvc()
+                .AddJsonOptions(o => {
+                    //o.JsonSerializerOptions.DictionaryKeyPolicy = null;
+                    o.JsonSerializerOptions.PropertyNamingPolicy = null;
+                    o.JsonSerializerOptions.WriteIndented = true;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
